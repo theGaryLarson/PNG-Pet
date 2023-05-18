50c2339375145a4c691c530c1a95c5aecd5cfe46
theGaryLarson <gary.larson316@gmail.com>
2023-05-18 01:10:18 -0700

    renamed theme

--- app/src/main/java/com/example/pngduel/ui/theme/Theme.kt
+++ app/src/main/java/com/example/pngduel/ui/theme/Theme.kt
@@ -38,7 +38,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun PNGDuelTheme(
+fun PNGPetTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
